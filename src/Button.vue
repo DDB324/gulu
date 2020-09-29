35b3c41894f21e0834d92e5576ba37247258b31b
ddb324 <jiangdaoran@163.com>
2020-09-30 00:59:30 +0800

    验证iconPosition的值是否为right或left

--- src/Button.vue
+++ src/Button.vue
@@ -1,11 +1,27 @@
 <template>
-  <div>
-    <button class="g-button">按钮</button>
-  </div>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"/>
+    </svg>
+    <span class="content">
+      <slot></slot>
+    </span>
+  </button>
 </template>
 
 <script lang='ts'>
-export default {};
+export default {
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator(value) {
+        return value === 'left' || value === 'right';
+      }
+    }
+  }
+};
 </script>
 
 <style lang='scss' scoped>
@@ -16,6 +32,20 @@ export default {};
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  //display: flex;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  vertical-align: middle;
+
+  .content {
+    order: 2;
+  }
+
+  .icon {
+    order: 1;
+    margin-right: .1em;
+  }
 
   &:hover {
     border-color: var(--border-color-hover)
@@ -28,7 +58,17 @@ export default {};
   &:focus {
     outline: none;
   }
+
+  &.icon-right {
+    .content {
+      order: 1;
+    }
+
+    .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
+  }
 }
-
-
 </style>
--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ import Vue from 'vue'
 import Button from "./Button";
 
 Vue.component('g-button', Button)
-console.log(Vue.component)
-let vm = new Vue({
+
+new Vue({
     el: '#app',
 })
 
-console.log(vm)
