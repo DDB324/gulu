dcb8907861561ec409386f3515fda5156238b0f6
ddb324 <jiangdaoran@163.com>
2020-10-02 22:42:12 +0800

    检测子元素是不是button

---
+++ src/Button-group.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  mounted() {
+    for (let node of this.$el.children) {
+      let name = node.nodeName.toLowerCase();
+      if (name !== 'button') {
+        console.warn(`g-button-group的子元素应该全是g-button,但是这个为${name}`);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.g-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+
+  .g-button {
+    border-radius: 0;
+
+    &:first-child {
+      border-bottom-left-radius: var(--border-radius);
+      border-top-left-radius: var(--border-radius)
+    }
+
+    &:last-child {
+      border-bottom-right-radius: var(--border-radius);
+      border-top-right-radius: var(--border-radius)
+    }
+
+    &:hover {
+      position: relative;
+      z-index: 1;
+    }
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from "./Button";
 import Icon from "./Icon";
+import ButtonGroup from './Button-group'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
     el: '#app',
