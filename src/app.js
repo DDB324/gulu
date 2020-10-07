ab643fd4c9d8de32f7a2d06c77235dd224893c6e
ddb324 <jiangdaoran@163.com>
2020-10-07 19:38:25 +0800

    使用chai.expect添加测试用例

--- src/app.js
+++ src/app.js
@@ -21,13 +21,76 @@ import chai from 'chai'
 const expect = chai.expect
 {
     const Constructor = Vue.extend(Button)
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings'
         }
     })
-    button.$mount('#test')
-    let useElement = button.$el.querySelector('use')
-    expect(useElement.getAttribute('xlink:href')).to.eq('settings')
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to
+        .eq('#i-settings')
+    vm.$el.remove()
+    vm.$destroy()
 }
-
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    expect(useElement.getAttribute('xlink:href')).to
+        .eq('#i-Loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    })
+    vm.$mount(div)
+    let svgElement = vm.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svgElement)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svgElement = vm.$el.querySelector('svg')
+    const {order} = window.getComputedStyle(svgElement)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    vm.$on('click', () => console.log('1'))
+    //希望函数被执行
+    let button = vm.$el
+    button.click()
+}
