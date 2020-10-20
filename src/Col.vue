9547f5c2217da9811e436929f0926463ba8405d5
ddb324 <jiangdaoran@163.com>
2020-10-21 00:49:21 +0800

    基本实现row 和col的功能

--- src/Col.vue
+++ src/Col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}">
+    <div style="border:1px solid green;height:100px">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,21 @@ export default {
   props: {
     span: {
       type: String | Number,
+    },
+    offset: {
+      type: String | Number,
+    }
+  },
+  data() {
+    return {
+      gutter: 0,
+    }
+  },
+  computed: {
+    getGutter() {
+      console.log(this.gutter);
+      console.log('1');
+      return this.gutter
     }
   }
 }
@@ -17,16 +35,21 @@ export default {
 
 <style lang="scss" scoped>
 .col {
-  height: 100px;
+  //height: 100px;
   width: 50%;
-  border: 1px solid red;
-  background: gray;
+  //border: 1px solid red;
+  //background: gray;
 
   $class-prefix: col-;
-
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24)*100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24)*100%;
     }
   }
 }
--- src/Row.vue
+++ src/Row.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
+  name: 'GuluRow',
+  props: {
+    gutter: {
+      type: String | Number,
+    }
+  },
+  mounted() {
+    this.$children.forEach((vm) => {
+      vm.gutter = this.gutter
+    })
+  }
 }
 </script>
 
 <style scoped lang="scss">
-.row{
+.row {
   display: flex;
 }
 </style>
