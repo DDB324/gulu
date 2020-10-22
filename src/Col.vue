1d758c22ca60fce4cf876688b7be9def0f1c30d1
ddb324 <jiangdaoran@163.com>
2020-10-22 19:28:22 +0800

    模拟淘宝页面

--- src/Col.vue
+++ src/Col.vue
@@ -6,15 +6,38 @@
 </template>
 
 <script>
+// let validator = (value) => {
+//   let keys = Object.keys(value)
+//   let valid = true
+//   keys.forEach(key => {
+//     if (!['span', 'offset'].includes(key)) {
+//       valid = false
+//     }
+//   })
+//   return valid
+// }
 export default {
   name: 'GuluCol',
   props: {
-    span: {
-      type: String | Number,
-    },
-    offset: {
-      type: String | Number,
-    }
+    span: {type: String | Number,},
+    offset: {type: String | Number,},
+    // phone: {
+    //   type: Object,
+    // validator(value) {
+    //   let keys = Object.keys(value)
+    //   let valid = true
+    //   keys.forEach(key => {
+    //     if (!['span', 'offset'].includes(key)) {
+    //       valid = false
+    //     }
+    //   })
+    //   return valid
+    // }
+    // },
+    // iPad: {type: Object, validator},
+    // narrowPc: {type: Object, validator},
+    // pc: {type: Object, validator},
+    // widePc: {type: Object, validator},
   },
   data() {
     return {
@@ -23,10 +46,20 @@ export default {
   },
   computed: {
     colClass() {
-      let {span, offset} = this
+      let {span, offset, phone, iPad, narrowPc, pc, widePc} = this
       return [
         span && `col-${span}`,
-        offset && `offset-${offset}`
+        offset && `offset-${offset}`,
+        // phone.span && `col-phone-${phone.span}`,
+        // phone.offset && `offset-phone-${phone.offset}`,
+        // iPad.span && `col-phone-${iPad.span}`,
+        // iPad.offset && `offset-phone-${iPad.offset}`,
+        // narrowPc.span && `col-phone-${narrowPc.span}`,
+        // narrowPc.offset && `offset-phone-${narrowPc.offset}`,
+        // pc.span && `col-phone-${pc.span}`,
+        // pc.offset && `offset-phone-${pc.offset}`,
+        // widePc.span && `col-phone-${widePc.span}`,
+        // widePc.offset && `offset-phone-${widePc.offset}`,
       ]
     },
     colStyle() {
@@ -42,8 +75,7 @@ export default {
 
 <style lang="scss" scoped>
 .col {
-  width: 50%;
-
+  white-space: nowrap;
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
@@ -54,6 +86,76 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       margin-left: ($n/24)*100%;
+    }
+  }
+  @media (max-width: 576px) {
+    $class-prefix: col-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-phone-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24)*100%;
+      }
+    }
+  }
+  @media (min-width: 577px) {
+    $class-prefix: col-iPad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-iPad-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24)*100%;
+      }
+    }
+  }
+  @media (max-width: 769px) {
+    $class-prefix: col-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-narrow-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24)*100%;
+      }
+    }
+  }
+  @media (max-width: 993px) {
+    $class-prefix: col-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24)*100%;
+      }
+    }
+  }
+  @media (max-width: 1201px) {
+    $class-prefix: col-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n/24)*100%;
+      }
+    }
+    $class-prefix: offset-wide-pc-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n/24)*100%;
+      }
     }
   }
 }
--- src/Row.vue
+++ src/Row.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="row" :style="rowStyle">
+  <div class="row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -10,6 +10,12 @@ export default {
   props: {
     gutter: {
       type: String | Number,
+    },
+    align: {
+      type: String,
+      validator(value) {
+        return ['left', 'right', 'center'].includes(value)
+      }
     }
   },
   mounted() {
@@ -24,6 +30,10 @@ export default {
         marginLeft: -gutter / 2 + 'px',
         marginRight: -gutter / 2 + 'px'
       }
+    },
+    rowClass() {
+      let {align} = this
+      return [align && `align-${align}`]
     }
   }
 }
@@ -32,5 +42,18 @@ export default {
 <style scoped lang="scss">
 .row {
   display: flex;
+  flex-wrap: nowrap;
+
+  &.align-left {
+    justify-content: flex-start;
+  }
+
+  &.align-center {
+    justify-content: center;
+  }
+
+  &.align-right {
+    justify-content: flex-end;
+  }
 }
 </style>
