c13e246fb0c83a81f1d21d19f256e2f6446a10bd
ddb324 <jiangdaoran@163.com>
2020-10-26 14:27:09 +0800

    sider组件加上基础的动画

--- src/Sider.vue
+++ src/Sider.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="sider">
-    <slot></slot>
-  </div>
+  <transition name="fade">
+    <div class="sider" v-if="visible">
+      <slot></slot>
+      <button @click="visible=!visible">close</button>
+    </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'GuluSider',
+  data() {
+    return {
+      visible: true
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 .sider {
+  position: relative;
 
+  button {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
