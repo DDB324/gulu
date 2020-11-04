eb74541862e1b31c233a9ce2c8b004f1bc797322
ddb324 <jiangdaoran@163.com>
2020-11-04 16:09:16 +0800

    创建popover组件

---
+++ src/Popover.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="popover" @click="xxx">
+    <div ref="content" class="content-wrapper" v-if="visible">
+      <slot name="content"></slot>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluPopover',
+  data() {
+    return {
+      visible: false
+    }
+  },
+  methods: {
+    xxx() {
+      this.visible = !this.visible
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.popover {
+  display: inline-block;
+  vertical-align: top;
+  position: relative;
+
+  .content-wrapper {
+    position: absolute;
+    border: 1px solid red;
+    bottom: 100%;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  }
+}
+</style>
--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ import TabsHead from "./Tabs-head";
 import TabsBody from "./Tabs-body";
 import TabsItem from "./Tabs-item";
 import TabsPane from "./Tabs-pane";
+import Popover from "./Popover";
 
 
 Vue.component('g-button', Button)
@@ -35,6 +36,7 @@ Vue.component('g-tabs-head', TabsHead)
 Vue.component('g-tabs-body', TabsBody)
 Vue.component('g-tabs-item', TabsItem)
 Vue.component('g-tabs-pane', TabsPane)
+Vue.component("g-popover", Popover)
 
 
 new Vue({
