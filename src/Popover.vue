c8e93da4ae31c529c40fc3624ae40740453e7163
ddb324 <jiangdaoran@163.com>
2020-11-06 02:12:30 +0800

    解决页面height超长的问题

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="popover" @click.stop="xxx">
-    <div ref="content" class="content-wrapper"
+    <div ref="contentWrapper" class="content-wrapper"
          @click.stop
          v-if="visible">
       <slot name="content"></slot>
     </div>
+    <span ref="triggerWrapper">
     <slot></slot>
+    </span>
   </div>
 </template>
 
@@ -22,14 +24,18 @@ export default {
       console.log('-----')
       this.visible = !this.visible
       if (this.visible === true) {
-        setTimeout(() => {
+        this.$nextTick(() => {
+          let {top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+          this.$refs.contentWrapper.style.top = top + scrollY + 'px'
+          this.$refs.contentWrapper.style.left = left + scrollX + 'px'
+          document.body.appendChild(this.$refs.contentWrapper)
           let eventHandler = () => {
             this.visible = false
             console.log('document隐藏了popover')
             document.removeEventListener('click', eventHandler)
           }
           document.addEventListener('click', eventHandler)
-        }, 0)
+        })
       } else {
         console.log('vm隐藏了popover')
       }
@@ -44,11 +50,11 @@ export default {
   vertical-align: top;
   position: relative;
 
-  .content-wrapper {
-    position: absolute;
-    border: 1px solid red;
-    bottom: 100%;
-    box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
-  }
+}
+
+.content-wrapper {
+  position: absolute;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.5);
+  transform: translateY(-100%);
 }
 </style>
