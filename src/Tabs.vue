85b1d997eaba11cb49755a45799610209df4211a
ddb324 <jiangdaoran@163.com>
2020-11-07 00:23:17 +0800

    重构tabs.vue

--- src/Tabs.vue
+++ src/Tabs.vue
@@ -32,19 +32,27 @@ export default {
       eventBus: this.eventBus
     }
   },
-  mounted() {
-    if(this.$children.length === 0){
-      console.warn('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
-    }
-    this.$children.forEach(vm => {
-      if (vm.$options.name === 'GuluTabsHead') {
-        vm.$children.forEach(childVm => {
-          if (childVm.$options.name === 'GuluTabsItem' && this.selected === childVm.name) {
-            this.eventBus.$emit('update:selected', this.selected, childVm)
-          }
-        })
+  methods: {
+    checkChildren() {
+      if (this.$children.length === 0) {
+        console.warn('tabs的子组件应该是tabs-head和tabs-body,但你没有写子组件')
       }
-    })
+    },
+    selectTab() {
+      this.$children.forEach(vm => {
+        if (vm.$options.name === 'GuluTabsHead') {
+          vm.$children.forEach(childVm => {
+            if (childVm.$options.name === 'GuluTabsItem' && this.selected === childVm.name) {
+              this.eventBus.$emit('update:selected', this.selected, childVm)
+            }
+          })
+        }
+      })
+    },
+  },
+  mounted() {
+    this.checkChildren()
+    this.selectTab()
   },
 
 }
