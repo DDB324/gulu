0a56d4357b06e8ba7e997cc0b3ea429fa3e97c0f
ddb324 <jiangdaoran@163.com>
2020-11-08 02:01:55 +0800

    支持click和hover两种事件触发

--- src/Popover.vue
+++ src/Popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click="onClick" ref="popover">
+  <div class="popover" ref="popover">
     <div ref="contentWrapper" class="content-wrapper"
          v-if="visible"
          :class="{[`position-${position}`]:true}">
@@ -19,12 +19,35 @@ export default {
       visible: false
     }
   },
+  mounted() {
+    if (this.trigger === 'click') {
+      this.$refs.popover.addEventListener('click', this.onClick)
+    } else if (this.trigger === 'hover') {
+      this.$refs.popover.addEventListener('mouseenter', this.open)
+      this.$refs.popover.addEventListener('mouseleave', this.close)
+    }
+  },
+  destroyed() {
+    if (this.trigger === 'click') {
+      this.$refs.popover.removeEventListener('click', this.onClick)
+    } else if (this.trigger === 'hover') {
+      this.$refs.popover.removeEventListener('mouseenter', this.open)
+      this.$refs.popover.removeEventListener('mouseleave', this.close)
+    }
+  },
   props: {
     position: {
       type: String,
       default: 'top',
       validator(value) {
         return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+      }
+    },
+    trigger: {
+      type: String,
+      default: 'click',
+      validator(value) {
+        return ['click', 'hover'].indexOf(value) >= 0;
       }
     }
   },
