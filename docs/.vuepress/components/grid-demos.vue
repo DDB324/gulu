30c668765129228286ee97603a95a2a246450e26
ddb324 <jiangdaoran@163.com>
2020-11-15 23:39:23 +0800

    基本完成官网grid内容

--- docs/.vuepress/components/grid-demos.vue
+++ docs/.vuepress/components/grid-demos.vue
@@ -1,13 +1,126 @@
 <template>
   <div>
-    <p>预览</p>
-    <g-row gutter="2">
-      <g-col span="12"></g-col>
-      <g-col span="10" offset="2"></g-col>
+    <p><strong>预览</strong></p>
+    <g-row class="demoRow">
+      <g-col span="12">
+        <div class="demoCol">12</div>
+      </g-col>
+      <g-col span="12">
+        <div class="demoCol">12</div>
+      </g-col>
     </g-row>
+    <g-row class="demoRow">
+      <g-col span="8">
+        <div class="demoCol">8</div>
+      </g-col>
+      <g-col span="8">
+        <div class="demoCol">8</div>
+      </g-col>
+      <g-col span="8">
+        <div class="demoCol">8</div>
+      </g-col>
+    </g-row>
+    <g-row class="demoRow">
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+    </g-row>
+    <g-row class="demoRow">
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+    </g-row>
+    <g-row class="demoRow">
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+    </g-row>
+    <g-row class="demoRow">
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+    </g-row>
+    <p><strong>代码</strong></p>
+    <pre><code>{{ content }}</code></pre>
   </div>
-  <p>代码</p>
-  <pre><code>{{ content }}</code></pre>
 </template>
 <script>
 import Row from '../../../src/Row'
@@ -20,13 +133,155 @@ export default {
   },
   data() {
     return {
-      content:`
-       <g-row gutter="2">
-      <g-col span="12"></g-col>
-      <g-col span="10" offset="2"></g-col>
+      content: `
+      * {
+          box-sizing: border-box;
+      }
+
+    <g-row class="demoRow">
+      <g-col span="12">
+        <div class="demoCol">12</div>
+      </g-col>
+      <g-col span="12">
+        <div class="demoCol">12</div>
+      </g-col>
     </g-row>
-      `.replace(/  +/g, '').trim(),
+
+    <g-row class="demoRow">
+      <g-col span="8">
+        <div class="demoCol">8</div>
+      </g-col>
+      <g-col span="8">
+        <div class="demoCol">8</div>
+      </g-col>
+      <g-col span="8">
+        <div class="demoCol">8</div>
+      </g-col>
+    </g-row>
+
+    <g-row class="demoRow">
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+      <g-col span="6">
+        <div class="demoCol">6</div>
+      </g-col>
+    </g-row>
+
+    <g-row class="demoRow">
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+      <g-col span="4">
+        <div class="demoCol">4</div>
+      </g-col>
+    </g-row>
+
+    <g-row class="demoRow">
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+      <g-col span="3">
+        <div class="demoCol">3</div>
+      </g-col>
+    </g-row>
+
+    <g-row class="demoRow">
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+      <g-col span="2">
+        <div class="demoCol">2</div>
+      </g-col>
+    </g-row>
+      `.replace(/ {4}(?! )/g,'').trim()
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+* {
+  box-sizing: border-box;
+}
+
+.demoRow {
+  margin: 10px 0;
+
+  .demoCol {
+    border: 1px solid #ccc;
+    height: 50px;
+    background: #eee;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+}
+
+</style>
--- docs/components/grid.md
+++ docs/components/grid.md
@@ -3,4 +3,26 @@ title:grid
 ---
 # 网格系统
 
-## 使用方法
+## 设置span(24栅格)
+
+<ClientOnly> 
+<grid-demos></grid-demos>
+</ClientOnly>
+
+## 设置gutter(col之间的间距)
+
+<ClientOnly> 
+<grid-gutter-demos></grid-gutter-demos>
+</ClientOnly>
+
+## 设置offset(空隙)
+
+<ClientOnly> 
+<grid-offset-demos></grid-offset-demos>
+</ClientOnly>
+
+## 设置align(row的位置)
+
+<ClientOnly> 
+<grid-align-demos></grid-align-demos>
+</ClientOnly>
