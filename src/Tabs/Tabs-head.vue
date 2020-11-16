d067a6ede26fbc51a471d5ef522fbfa034864921
ddb324 <jiangdaoran@163.com>
2020-11-16 21:48:19 +0800

    修改横线显示位置不对的bug

--- src/Tabs/Tabs-head.vue
+++ src/Tabs/Tabs-head.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tabs-head">
+  <div class="tabs-head" ref="head">
     <slot></slot>
     <div class="line" ref="line"></div>
     <div class="actions-wrapper">
@@ -15,8 +15,9 @@ export default {
   mounted() {
     this.eventBus.$on('update:selected', (item, vm) => {
       let {width, left} = vm.$el.getBoundingClientRect()
+      let {left: left2} = this.$refs.head.getBoundingClientRect()
       this.$refs.line.style.width = `${width}px`
-      this.$refs.line.style.left = `${left}px`
+      this.$refs.line.style.left = `${left-left2}px`
     })
   }
 }
