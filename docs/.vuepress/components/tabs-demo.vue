254ec98d0186df225ae7b8fe17b3c903b7da8e30
ddb324 <jiangdaoran@163.com>
2020-11-18 02:32:26 +0800

    优化网页tabs内容

---
+++ docs/.vuepress/components/tabs-demo.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <p><strong>预览</strong></p>
+    <d-tabs selected="news">
+      <d-tabs-head>
+        <d-tabs-item name="news">新闻</d-tabs-item>
+        <d-tabs-item name="videos">视频</d-tabs-item>
+        <d-tabs-item name="pictures">图片</d-tabs-item>
+      </d-tabs-head>
+      <d-tabs-body>
+        <d-tabs-pane name="news">新闻的资讯</d-tabs-pane>
+        <d-tabs-pane name="videos">视频的资讯</d-tabs-pane>
+        <d-tabs-pane name="pictures">图片的资讯</d-tabs-pane>
+      </d-tabs-body>
+    </d-tabs>
+    <p><strong>代码</strong></p>
+    <pre><code>{{ content }}</code></pre>
+  </div>
+</template>
+
+<script>
+import Tabs from "../../../src/Tabs/Tabs";
+import TabsBody from "../../../src/Tabs/Tabs-body";
+import TabsHead from "../../../src/Tabs/Tabs-head";
+import TabsItem from "../../../src/Tabs/Tabs-item";
+import TabsPane from "../../../src/Tabs/Tabs-pane";
+
+export default {
+  components: {
+    'd-tabs': Tabs,
+    'd-tabs-body': TabsBody,
+    'd-tabs-head': TabsHead,
+    'd-tabs-item': TabsItem,
+    'd-tabs-pane': TabsPane,
+  },
+  data() {
+    return {
+      content: `
+     <d-tabs selected="news">
+      <d-tabs-head>
+        <d-tabs-item name="news">新闻</d-tabs-item>
+        <d-tabs-item name="videos">视频</d-tabs-item>
+        <d-tabs-item name="pictures">图片</d-tabs-item>
+      </d-tabs-head>
+      <d-tabs-body>
+        <d-tabs-pane name="news">新闻的资讯</d-tabs-pane>
+        <d-tabs-pane name="videos">视频的资讯</d-tabs-pane>
+        <d-tabs-pane name="pictures">图片的资讯</d-tabs-pane>
+      </d-tabs-body>
+    </d-tabs>
+      `.replace(/ {4}(?! )/g, '').trim()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+*{
+}
+</style>
---
+++ docs/pages/components/tabs.md
@@ -0,0 +1,12 @@
+# Tabs 标签
+
+## 支持HTML
+
+<ClientOnly> 
+<tabs-demo></tabs-demo>
+</ClientOnly>
+
+#### 1.此组件的 d-tabs d-tabs-item d-tabs-pane 必须有一一对应的 name。
+#### 2.默认高亮状态 d-tabs 需要设置 selected 属性来确定默认标签。
+#### 3.该标签的子组件应该是 d-tabs-head 和 d-tabs-body ，否则你会得到一个警告。
+#### 4.disabled 状态 disabled 属性用于 i-tabs-item 标签，可以设置该标签的不可用状态。
