ac987eabdf06bf143cf398c5e054bad7f062a8f6
ddb324 <jiangdaoran@163.com>
2020-11-18 20:19:52 +0800

    修复网页中顶部导航栏文档的导航内容错误

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,17 +16,19 @@ module.exports = {
             //格式一：直接跳转，'/'为不添加路由，跳转至首页
             {text: '主页', link: '/'},
 
-            {text: '文档', link: '/pages/install/README.md'},
+            {text: '文档', link: '/pages/install/install'},
 
             //格式三：跳转至外部网页，需http/https前缀
             {text: 'Github', link: 'https://github.com/DDB324/gulu'},
         ],
+        //侧边导航栏：会根据当前的文件路径是否匹配侧边栏数据，自动显示/隐藏
         sidebar: [
             {
-                title: '入门',
-                collapsable: false,
+                title: '入门',  // 一级菜单名称
+                collapsable: false, // false为默认展开菜单, 默认值true是折叠
+                sidebarDepth: 1,    //  设置侧边导航自动提取markdown文件标题的层级，默认1为h2层级
                 children: [
-                    ['/pages/install/install','安装'],
+                    ['/pages/install/install','安装'],  //菜单名称为'安装'，跳转至/pages/install/install.md,md可以省略
                     ['/pages/get-started/getStarted','快速上手'],
                 ]
             },
