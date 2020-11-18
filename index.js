2413eca66e64b4201e0ebb1ae957e1e53b746218
ddb324 <jiangdaoran@163.com>
2020-11-19 01:07:15 +0800

    准备发布npm包

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 import Button from './src/Button/Button'
 import ButtonGroup from './src/Button/Button-group'
 import Icon from './src/Icon/Icon'
+import Input from "./src/Input/Input";
+import Col from "./src/Grid/Col";
+import Row from "./src/Grid/Row";
+import Content from "./src/Layout/Content";
+import Header from "./src/Layout/Header";
+import Footer from "./src/Layout/Footer";
+import Layout from "./src/Layout/Layout";
+import Sider from "./src/Layout/Sider";
+import plugin from "./src/plugin";
+import Tabs from "./src/Tabs/Tabs";
+import TabsHead from "./src/Tabs/Tabs-head";
+import TabsBody from "./src/Tabs/Tabs-body";
+import TabsItem from "./src/Tabs/Tabs-item";
+import TabsPane from "./src/Tabs/Tabs-pane";
+import Popover from "./src/Popover/Popover";
+import Collapse from "./src/Collapse/Collapse";
+import CollapseItem from "./src/Collapse/Collapse-item";
 
-export {Button, ButtonGroup, Icon}
+
+export {Button, ButtonGroup, Icon,Input,Col,Row,Content,Header,Footer,Layout,Sider,plugin,Tabs,TabsHead,TabsBody,TabsItem,TabsPane,Popover,Collapse,CollapseItem}
