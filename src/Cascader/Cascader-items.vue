875f3e6a1ec02ff8b3e6bb573b6fa1ad62574010
ddb324 <jiangdaoran@163.com>
2020-11-26 00:11:33 +0800

    使用递归组件渲染内容

---
+++ src/Cascader/Cascader-items.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cascaderItems">
+    <div class="left">
+      <div class="label" v-for="item in sourceItem" @click="leftSelected=item">
+        {{ item.name }}
+      </div>
+    </div>
+    <div class="right" v-if="rightSelected">
+      <GuluCascaderItem :source-item="rightSelected"></GuluCascaderItem>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuluCascaderItem',
+  computed:{
+    rightSelected(){
+      if(this.leftSelected && this.leftSelected.children){
+        return this.leftSelected.children
+      }else{
+        return null
+      }
+    }
+  },
+  props: {
+    sourceItem: {
+      type: Array,
+    }
+  },
+  data(){
+    return {
+      leftSelected: null,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.cascaderItems{
+  display:flex;
+  .left{
+    outline:1px solid red;
+  }
+}
+</style>
